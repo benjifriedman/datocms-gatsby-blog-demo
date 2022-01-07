4e5da32464253abfc3ce7a5b9f18e2a2ee58ddf5
Benji Friedman <benjifriedman@gmail.com>
2022-01-06 22:43:19 -0800

    maybe this will work?

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Blog Demo",
+    title: "benjifriedman.com",
   },
   plugins: [
     "gatsby-plugin-postcss",
--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,12 @@
 import React from "react";
-import { Link } from 'gatsby'
+import { Link } from "gatsby";
 
 export default function Header() {
   return (
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
       <Link to="/" className="hover:underline">
-        Blog
+        Benji Friedman
       </Link>
-      .
     </h2>
-  )
+  );
 }
